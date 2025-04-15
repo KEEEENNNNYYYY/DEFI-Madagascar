736008c7acefbf6acf80d493abeef7f5c2003ef3
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-04-15 17:09:14 +0200

    feat : render config

--- Web-app/front-end/vite.config.js
+++ Web-app/front-end/vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',            // 👈 nécessaire pour Render
+    port: process.env.PORT || 5173, // 👈 Render fournit le port dans cette variable
+  },
 })
