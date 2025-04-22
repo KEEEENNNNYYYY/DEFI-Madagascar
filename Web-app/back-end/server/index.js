174ddc04f2c749c066aa981b5fc3c58001479cce
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-04-22 10:19:59 +0200

    feat : new endpoint

--- Web-app/back-end/server/index.js
+++ Web-app/back-end/server/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 require('dotenv').config({ path: '../config/.env' });
 const pool = require('../config/db');
 const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const server = http.createServer(app); 
+const io = socketIo(server);
 
 app.use(cors());
 app.use(express.json());
@@ -15,13 +19,15 @@ const createUser = require('./request/userRequest/createUser');
 const findUser = require('./request/userRequest/findUser');
 const getUserById = require('./request/userRequest/getUserById');
 
-
 const getAllPost = require('./request/postRequest/getAllPost');
 const createPost = require('./request/postRequest/createPost');
 const deletePost = require('./request/postRequest/deletePost');
 const getUserPosts = require('./request/postRequest/getUserPost');
 const getPostById = require('./request/postRequest/getPostById');
 
+const sendMessage = require('./request/messageRequest/sendMessage');
+const getDiscussion = require('./request/messageRequest/getDiscussion')
+const getAllDiscussion = require('./request/messageRequest/getAllDiscussion')
 
 // Routes
 app.get('/', (req, res) => {
@@ -38,6 +44,11 @@ app.use('/posts', getPostById);
 app.use('/posts', getAllPost);
 app.use('/posts', createPost);
 app.use('/posts', deletePost);
+
+app.use('/message',sendMessage);
+app.use('/message',getDiscussion);
+app.use('/chat',getAllDiscussion);
+
 
 
 // Lancement du serveur
